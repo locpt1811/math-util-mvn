2b004d9f00fb704724352bf25a72539121d35ea7
locpt1811 <phamtanloc18112003@gmail.com>
2023-06-26 16:50:48 +0700

    update code to use recursion

--- src/main/java/org/locpt/mathutil/core/MathUtility.java
+++ src/main/java/org/locpt/mathutil/core/MathUtility.java
@@ -27,6 +27,21 @@ public class MathUtility {
     --> Hàm chỉ cho phép 0 =< n =< 20 
         n > 20 --> exception
     */
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be between 0...20, please!!!!");
+//        
+//        if(n == 0 || n == 1)
+//            return 1;
+//        
+//        long product =1; // tich cac bien 
+//        
+//        for (int i = 2; i <= n; i++)
+//            product *= i;
+//        
+//        return product;
+//    }
+    
     public static long getFactorial(int n){
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0...20, please!!!!");
@@ -34,11 +49,15 @@ public class MathUtility {
         if(n == 0 || n == 1)
             return 1;
         
-        long product =1; // tich cac bien 
+        return n * getFactorial(n - 1);
         
-        for (int i = 2; i <= n; i++)
-            product *= i;
         
-        return product;
     }
 }
+
+//5! = 1*2*3*4*5 = 5! * 4
+//4! = 4*3!
+//3! = 3*2!
+//2! = 2*1!
+//1! = 1 STOP
+//N! = N * (N-1)!
